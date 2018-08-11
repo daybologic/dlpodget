63bc266291fbcae8638a4ef0f60e9c8efddebf55
Duncan Ross Palmer <palmer@overchat.org>
2018-08-11 11:26:35 +0000

    Move OurConfig into Dlpodget::Config

---
+++ lib/Dlpodget/Config.pm
@@ -0,0 +1,90 @@
+#
+# Daybo Logic Podcast downloader
+# Copyright (c) 2012-2018, Duncan Ross Palmer (2E0EOL) and others,
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#     * Neither the name of the Daybo Logic nor the names of its contributors
+#       may be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+package Dlpodget::Config;
+
+use Moose;
+use Config::IniFiles;
+
+sub new {
+	my $class = shift;
+	my $obj = {
+		'ini' => undef
+	};
+
+	$obj = bless($obj, $class);
+	return $obj;
+}
+
+sub initConfigDefaults($) {
+	my $Feeds = shift;
+	my $confmain = $Feeds->{main};
+	die 'Assertion failure' unless $confmain;
+
+	$confmain->{TMPDIR} = '/tmp' unless ( $confmain->{TMPDIR} );
+	$confmain->{HOME} = $confmain->{TMPDIR} unless ( $confmain->{HOME} );
+	$confmain->{LOCALPFX} = $confmain->{HOME} unless ( $confmain->{LOCALPFX} );
+
+	my %defaults = (
+		'enable' => 1,
+		'noop'   => 0,
+		'debug'  => 0
+	);
+
+	foreach my $opt ( keys(%defaults) ) {
+		next if ( defined($confmain->{$opt}) && $confmain->{$opt} =~ m/\d$/ );
+		$confmain->{$opt} = $defaults{$opt};
+	}
+}
+
+sub initFeedDefaults($$) {
+	my ( $Feeds, $Feed ) = @_;
+	$Feed->{localpath} = $Feeds->{main}->{LOCALPFX} unless ( $Feed->{localpath} );
+	$Feed->{rss} = '' unless ( $Feed->{rss} );
+	foreach my $chk ( 'check', 'download', 'enable' ) {
+		$Feed->{$chk} = 1 if ( !$Feed->{$chk} || $Feed->{$chk} !~ m/\^d$/ );
+	}
+}
+
+sub load($) {
+	my $self = shift;
+	foreach my $confFile ( @confFiles ) {
+		next unless ( -f $confFile );
+		$self->{'ini'} = Config::IniFiles->new(-file => $confFile, -commentchar => ';');
+		if ( !$self->{'ini'} ) {
+			print(STDERR "Fault with $confFile: " . join(',', @Config::IniFiles::errors) . "\n");
+			return 1;
+		}
+		last;
+	}
+}
+
+1;
