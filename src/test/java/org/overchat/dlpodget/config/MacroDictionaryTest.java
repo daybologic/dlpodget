88be37ee02ccd4f454e3d106dd0f8b9646b53b2c
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2024-03-08 23:22:54 +0000

    ➕ Initial JUnit code

---
+++ src/test/java/org/overchat/dlpodget/config/MacroDictionaryTest.java
@@ -0,0 +1,16 @@
+package org.overchat.dlpodget.config;
+
+import org.junit.jupiter.api.Test;
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+
+//import org.junit.Test;
+
+public class MacroDictionaryTest {
+
+	@Test
+	public void testNothingToDo() {
+		//final String input = "Y5XBx9ThW1wSCXLYidaFiULbABnsaCuz";
+		//assertEquals("Nothing changed", input, sut.resolve(input));
+	}
+}
