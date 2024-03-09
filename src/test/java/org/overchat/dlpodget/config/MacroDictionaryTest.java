0dbdae93db4e5ecf79091b1fdee8dc4330f3f827
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2024-03-09 10:01:10 +0000

    ✅ First Java ☕️ test working
    This test just shows that the parser doesn't do any translations
    when there is nothing to do.

--- src/test/java/org/overchat/dlpodget/config/MacroDictionaryTest.java
+++ src/test/java/org/overchat/dlpodget/config/MacroDictionaryTest.java
@@ -1,16 +1,24 @@
 package org.overchat.dlpodget.config;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
 
-//import org.junit.Test;
+//import org.junit.Assert.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MacroDictionaryTest {
 
+	private MacroDictionary sut;
+
+	@BeforeEach
+	public void setUp() {
+		sut = new MacroDictionary();
+	}
+
 	@Test
 	public void testNothingToDo() {
-		//final String input = "Y5XBx9ThW1wSCXLYidaFiULbABnsaCuz";
-		//assertEquals("Nothing changed", input, sut.resolve(input));
+		final String input = "Y5XBx9ThW1wSCXLYidaFiULbABnsaCuz";
+		assertEquals(input, sut.resolve(input), "Nothing changed");
 	}
 }
